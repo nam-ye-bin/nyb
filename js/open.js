b70f0d795b9f5a84df5baba999bb2be02fe04e27
nam-ye-bin <92584966+nam-ye-bin@users.noreply.github.com>
2021-10-21 21:17:54 +0900

    Add files via upload

---
+++ js/open.js
@@ -0,0 +1,88 @@
+﻿ $(function(){
+
+        //1번째 이미지
+        $('.btn1').click(function(){
+          window.open("img/101.jpg","blank","width=1200, height=850")
+        });
+
+
+
+        //2번째 이미지
+        $('.btn2').click(function(){
+          window.open("img/102.jpg","blank","width=1020, height=720")
+        });
+
+
+
+
+        //3번째 이미지
+        $('.btn3').click(function(){
+          window.open("img/103.jpg","blank","width=800, height=900")
+        });
+
+
+
+
+        //4번째 이미지
+         $('.btn4').click(function(){
+          window.open("img/105.jpg","blank","width=1200, height=850")
+        });
+
+
+
+
+        //5번째 이미지
+       $('.btn5').click(function(){
+          window.open("img/104.jpg","blank","width=1020, height=725")
+        });
+
+
+
+
+
+        //6번째 이미지
+        $('.btn6').click(function(){
+          window.open("img/106.jpg","blank","width=1020, height=725")
+        });
+
+
+
+
+        //7번째 이미지
+        $('.btn7').click(function(){
+          window.open("img/107.jpg","blank","width=1200, height=835")
+        });
+
+
+
+
+        //8번째 이미지
+        $('.btn8').click(function(){
+          window.open("img/108.jpg","blank","width=1200, height=850")
+        });
+
+
+
+
+        //9번째 이미지
+        $('.btn9').click(function(){
+          window.open("img/109.jpg","blank","width=1200, height=845")
+        });
+
+
+
+
+        //10번째 이미지
+        $('.btn10').click(function(){
+          window.open("img/110.jpg","blank","width=800, height=900")
+        });
+
+
+
+
+        //11번째 이미지
+        $('.btn11').click(function(){
+          window.open("img/111.jpg","blank","width=700, height=850")
+        });
+
+      });
